1d45c5f0c8f8511753bb86cfbd6e06ccb11dd37b
Clickism <yagizaktasx@gmail.com>
2026-09-01 17:49:03 +0200

    add char wrapping

--- src/main/java/de/clickism/clickui/elements/Text.java
+++ src/main/java/de/clickism/clickui/elements/Text.java
@@ -12,6 +12,7 @@ import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FormattedText;
 import net.minecraft.network.chat.Style;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.util.StringDecomposer;
 
 import java.util.ArrayList;
@@ -32,6 +33,10 @@ public class Text extends UiElement<Text> implements Wrappable {
      * The alignment of the text within.
      */
     private Align align = Align.LEFT;
+    /**
+     * The wrap mode of the text. Determines how the text should be wrapped when it exceeds the available width.
+     */
+    private WrapMode wrapMode = WrapMode.WORD;
 
     /**
      * Creates a new Text element with the specified text.
@@ -107,13 +112,46 @@ public class Text extends UiElement<Text> implements Wrappable {
         return alignText(Align.RIGHT);
     }
 
+    /**
+     * Sets the wrap mode of the Text element to word wrapping.
+     *
+     * @return The current Text element instance.
+     */
+    public Text wrapWords() {
+        this.wrapMode = WrapMode.WORD;
+        this.invalidateLayout();
+        return this;
+    }
+
+    /**
+     * Sets the wrap mode of the Text element to character wrapping.
+     *
+     * @return The current Text element instance.
+     */
+    public Text wrapCharacters() {
+        this.wrapMode = WrapMode.CHARACTER;
+        this.invalidateLayout();
+        return this;
+    }
+
+    /**
+     * Sets the wrap mode of the Text element to no wrapping.
+     *
+     * @return The current Text element instance.
+     */
+    public Text noWrap() {
+        this.wrapMode = WrapMode.NONE;
+        this.invalidateLayout();
+        return this;
+    }
+
     @Override
     public Size intrinsicSize() {
         var fontScale = this.resolvedStyle().get(StyleProperty.FONT_SCALE);
         var width = lines.stream()
-            .mapToDouble(line -> Util.font().width(line))
-            .max()
-            .orElse(0) * fontScale;
+                        .mapToDouble(line -> Util.font().width(line))
+                        .max()
+                        .orElse(0) * fontScale;
         var height = Util.font().lineHeight * fontScale * lines.size();
         // Ceil the size to ensure it fits within the bounds
         return new Size((int) Math.ceil(width), (int) Math.ceil(height));
@@ -121,12 +159,43 @@ public class Text extends UiElement<Text> implements Wrappable {
 
     @Override
     public Size defaultMinSize() {
-        var splitter = Util.font().getSplitter();
+        if (wrapMode == WrapMode.NONE) {
+            return intrinsicSize();
+        }
+        var maxWidth = switch (wrapMode) {
+            case WORD -> maxWordWidth();
+            case CHARACTER -> maxCharWidth();
+            // Unreachable
+            case NONE -> throw new IllegalStateException();
+        };
         var scale = resolvedStyle().get(StyleProperty.FONT_SCALE);
+        var lineHeight = Util.font().lineHeight;
 
+        var minWidth = (int) Math.ceil(maxWidth * scale);
+        var minHeight = (int) Math.ceil(lineHeight * scale);
+
+        // Add padding to the minimum size
+        minWidth += this.padding().horizontal();
+        minHeight += this.padding().vertical();
+
+        return new Size(minWidth, minHeight);
+    }
+
+    private double maxCharWidth() {
+        var maxCharWidth = new AtomicDouble(0);
+        StringDecomposer.iterateFormatted(text, Style.EMPTY, (index, style, codePoint) -> {
+            var charWidth = Util.font().width(FormattedCharSequence.codepoint(codePoint, style));
+            maxCharWidth.getAndUpdate(value -> Math.max(value, charWidth));
+            return true;
+        });
+        return maxCharWidth.get();
+    }
+
+    private double maxWordWidth() {
+        // For word wrapping, we need to find the maximum word width
+        var splitter = Util.font().getSplitter();
         final var currentWord = new ArrayList<FormattedText>();
         final var maxWordWidth = new AtomicDouble(0);
-
         // Iterate over the text and find the maximum word widthx
         StringDecomposer.iterateFormatted(text, Style.EMPTY, (index, style, codePoint) -> {
             if (codePoint == ' ' || codePoint == '\n') {
@@ -144,39 +213,71 @@ public class Text extends UiElement<Text> implements Wrappable {
             ));
             return true;
         });
-
         // Handle the last word if the text doesn't end with a space or newline
         if (!currentWord.isEmpty()) {
             var word = FormattedText.composite(currentWord);
             maxWordWidth.getAndUpdate(value ->
                 Math.max(value, splitter.stringWidth(word)));
         }
-
-        var height = Util.font().lineHeight * scale;
-
-        var minWidth = (int) Math.ceil(maxWordWidth.get() * scale);
-        var minHeight = (int) Math.ceil(height);
-
-        // Add padding to the minimum size
-        minWidth += this.padding().horizontal();
-        minHeight += this.padding().vertical();
-
-        return new Size(minWidth, minHeight);
+        return maxWordWidth.get();
     }
 
-    // TODO: Wrap by character if a single word is too long to fit within the maxWidth
     @Override
     public void wrap(int maxWidth) {
+        // Wrap the text based on the specified wrap mode and maximum width
         var font = Util.font();
-        this.lines = font
-            .getSplitter()
-            .splitLines(text, maxWidth, Style.EMPTY);
+        this.lines = switch (wrapMode) {
+            case NONE -> List.of(text);
+            case WORD -> font
+                .getSplitter()
+                .splitLines(text, maxWidth, Style.EMPTY);
+            case CHARACTER -> wrapCharacters(maxWidth);
+        };
         // Calculate new height
         var fontScale = this.resolvedStyle().get(StyleProperty.FONT_SCALE);
         var height = font.lineHeight * fontScale * lines.size();
         height += this.padding().vertical();
         // Update bounds with new height
         this.bounds(this.bounds().withHeight((int) Math.ceil(height)));
+    }
+
+    private List<FormattedText> wrapCharacters(int maxWidth) {
+        var lines = new ArrayList<FormattedText>();
+        var current = new ArrayList<FormattedText>();
+        var width = new AtomicDouble(0d);
+
+        StringDecomposer.iterateFormatted(text, Style.EMPTY, (index, style, codePoint) -> {
+            if (codePoint == '\n') {
+                lines.add(FormattedText.composite(current));
+                current.clear();
+                width.set(0);
+                return true;
+            }
+
+            var charWidth = Util.font().width(FormattedCharSequence.codepoint(codePoint, style));
+
+            if (!current.isEmpty() && width.get() + charWidth > maxWidth) {
+                lines.add(FormattedText.composite(current));
+                current.clear();
+                width.set(0);
+            }
+
+            current.add(
+                FormattedText.of(
+                    new String(Character.toChars(codePoint)),
+                    style
+                )
+            );
+            width.updateAndGet(v -> v + charWidth);
+
+            return true;
+        });
+
+        if (!current.isEmpty()) {
+            lines.add(FormattedText.composite(current));
+        }
+
+        return lines;
     }
 
     @Override
@@ -218,5 +319,14 @@ public class Text extends UiElement<Text> implements Wrappable {
         LEFT,
         CENTER,
         RIGHT
+    }
+
+    /**
+     * Text wrap mode enum.
+     */
+    public enum WrapMode {
+        NONE,
+        WORD,
+        CHARACTER
     }
 }
